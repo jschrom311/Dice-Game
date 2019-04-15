0fb72c1475ef37a474c3a9d2653c408671ca1603
jschrom311 <jschrom311@yahoo.com>
2019-04-15 17:44:09 -0500

    input functionality added

--- app.js
+++ app.js
@@ -70,9 +70,19 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         //update UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+
+        //Undefined, 0, null, or "" are COERCED to false; anything else COERCED to true
+        if(input) {
+            winningScore = input;
+        }
+        else {
+            winningScore = 100;
+        }
 
         //check if player won
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
